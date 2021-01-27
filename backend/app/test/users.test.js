fb5992c633ca6d062eae1cc0a6023e924636c13d
bottlehs <qudgns9@gmail.com>
2021-01-27 18:56:53 +0900

    jest users add

--- backend/app/test/users.test.js
+++ backend/app/test/users.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const app = require('../../server.jest');
 require("../routes/users.routes")(app);
+const { v4: uuidv4 } = require('uuid');
 
 jest.mock("./save_json", () => ({
   save: jest.fn(),
@@ -20,6 +21,22 @@ jest.mock("./usStates.json", () => [
 ]);
 
 describe("testing-server-users-routes", () => {
+  it("POST /api/users - success", async (done) => {
+    const params = {
+      firstname: 'firstname',
+      lastname: 'lastname',
+      username: 'username',
+      email: uuidv4()+'@email.com',
+      password: 'password123',      
+    };
+
+    const response = await request(app).post("/api/users").send(params);
+    expect(response.statusCode).toEqual(200);    
+    done();
+
+    // TODO: https://medium.com/@pojotorshemi/integration-test-on-express-restful-apis-using-jest-and-supertest-4cf5d1414ab0 를 참고하여 save_json, usStates.json 활용하기
+  }),
+
   it("GET /api/users - success", async (done) => {
     const response = await request(app).get("/api/users");
     expect(response.statusCode).toEqual(200);
