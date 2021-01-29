50fd785bec8c9829ff14ecfc3280988a0fe1b270
bottlehs <qudgns9@gmail.com>
2021-01-29 21:56:47 +0900

    posts view

--- frontend/src/views/posts/Edit.vue
+++ frontend/src/views/posts/Edit.vue
@@ -31,10 +31,31 @@ export default {
        * id : 단건 식별자
        * item : 응답 데이터
        * wait : 로딩
+       * formWait : 폼 로딩
+       * form : 폼
        */      
       id: 0,
       wait: false,
-      item: {},      
+      formWait: false,
+      form: {
+        /**
+         * usersId: users id (후보키)
+         * title: 제목
+         * content: 내용
+         * status: 상태
+         * commentsStatus: 댓글 상태
+         * type: 유형
+         * commentsCount: 댓글수
+         */
+        usersId: "",
+        title: "",
+        content: "",
+        status: "",
+        commentsStatus: "",
+        type: "",
+        commentsCount: "",
+      },
+      item: {},
     };
   },
   created() {
@@ -74,6 +95,30 @@ export default {
           response => {
             const { data } = response;
             this.item = data;
+
+            // form
+            if (Object.prototype.hasOwnProperty.call(data,"usersId")) {
+              this.form.usersId = data.usersId;
+            };
+            if (Object.prototype.hasOwnProperty.call(data,"title")) {
+              this.form.title = data.title;
+            };
+            if (Object.prototype.hasOwnProperty.call(data,"content")) {
+              this.form.content = data.content;
+            };
+            if (Object.prototype.hasOwnProperty.call(data,"status")) {
+              this.form.status = data.status;
+            };
+            if (Object.prototype.hasOwnProperty.call(data,"commentsStatus")) {
+              this.form.commentsStatus = data.commentsStatus;
+            };
+            if (Object.prototype.hasOwnProperty.call(data,"type")) {
+              this.form.type = data.type;
+            };                 
+            if (Object.prototype.hasOwnProperty.call(data,"commentsCount")) {
+              this.form.commentsCount = data.commentsCount;
+            };
+
             this.wait = true;
           },
           error => {
