53cc9d927eb6c8ff44ee1333d4113404246fc6fd
bottlehs <qudgns9@gmail.com>
2021-01-29 21:56:07 +0900

    carts view

--- frontend/src/views/carts/Edit.vue
+++ frontend/src/views/carts/Edit.vue
@@ -31,9 +31,24 @@ export default {
        * id : 단건 식별자
        * item : 응답 데이터
        * wait : 로딩
+       * formWait : 폼전송
+       * form : 폼
        */      
       id: 0,
       wait: false,
+      formWait: false,
+      form: {        
+        /**
+         * usersId: users id (후보키)
+         * productsId: products id (후보키)
+         * productsOptionsId: productsOptions id (후보키)
+         * status: 상태
+         */
+        usersId: "",
+        productsId: "",
+        productsOptionsId: "",
+        status: ""
+      },
       item: {},      
     };
   },
@@ -74,6 +89,21 @@ export default {
           response => {
             const { data } = response;
             this.item = data;
+
+            // form
+            if (Object.prototype.hasOwnProperty.call(data,"usersId")) {
+              this.form.usersId = data.usersId;
+            };
+            if (Object.prototype.hasOwnProperty.call(data,"productsId")) {
+              this.form.productsId = data.productsId;
+            };            
+            if (Object.prototype.hasOwnProperty.call(data,"productsOptionsId")) {
+              this.form.productsOptionsId = data.productsOptionsId;
+            };            
+            if (Object.prototype.hasOwnProperty.call(data,"status")) {
+              this.form.status = data.status;
+            };
+
             this.wait = true;
           },
           error => {
