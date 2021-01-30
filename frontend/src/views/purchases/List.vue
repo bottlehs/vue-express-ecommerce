9071e021a5b85be8619b03ed01340cc9442a6402
bottlehs <qudgns9@gmail.com>
2021-01-30 15:23:41 +0900

    구매 데이터 셋팅

--- frontend/src/views/purchases/List.vue
+++ frontend/src/views/purchases/List.vue
@@ -14,9 +14,18 @@ export default {
   },
   data() {
     /**
-     * data
+       * search : 검색 데이터
+       * fields : 검색결과 페이지 리스트 필드
+       * items : 응답 리스트 데이터
+       * page : 검색결과 페이지 데이터
+       * wait : 로딩
+       * totalItems : 전체 데이터수
+       * totalPages : 전체 페이지수
+       * currentPage : 현제 페이지
+       * pageSize: 페이지 요청 데이터수
      */
-    return {};
+    return {
+    };
   },
   created() {
     /**
