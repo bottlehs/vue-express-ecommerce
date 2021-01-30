ffaa85b675935f2cd0f1d8e3cd52541dddf55eb4
bottlehs <qudgns9@gmail.com>
2021-01-30 15:30:47 +0900

    댓글 데이터 구조 셋팅

--- frontend/src/views/comments/List.vue
+++ frontend/src/views/comments/List.vue
@@ -13,18 +13,35 @@ export default {
      */
   },
   data() {
-    /**
-     * search : 검색 데이터
-     * fields : 검색결과 페이지 리스트 필드
-     * items : 응답 리스트 데이터
-     * page : 검색결과 페이지 데이터
-     * wait : 로딩
-     * totalItems : 전체 데이터수
-     * totalPages : 전체 페이지수
-     * currentPage : 현제 페이지
-     * pageSize: 페이지 요청 데이터수
-     */
-    return {};
+    return {
+      /**
+       * search : 검색 데이터
+       * fields : 검색결과 페이지 리스트 필드
+       * items : 응답 리스트 데이터
+       * page : 검색결과 페이지 데이터
+       * wait : 로딩
+       * totalItems : 전체 데이터수
+       * totalPages : 전체 페이지수
+       * currentPage : 현제 페이지
+       * pageSize: 페이지 요청 데이터수
+       */
+      wait: false,
+      search: {
+        /**
+         */
+      },
+      fields: [
+        {
+          key: 'temp',
+          label: 'temp'
+        }
+      ],
+      items: [],
+      totalItems: 0,
+      totalPages: 0,
+      currentPage: 0,
+      pageSize: 10,
+    };
   },
   created() {
     /**
